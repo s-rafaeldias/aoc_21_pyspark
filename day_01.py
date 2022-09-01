396ff4deac25b5ae67b54e7cc5cab9d49c5544be
Rafael Dias <s.rafaeldias@protonmail.com>
2022-08-31 23:09:39 -0300

    chore: renames

--- day_01.py
+++ day_01.py
@@ -1,6 +1,5 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as func
-from pyspark.sql import types
 from pyspark.sql.types import IntegerType, StructField, StructType
 from pyspark.sql.window import Window
 
@@ -14,12 +13,12 @@ with SparkSession.builder.appName("AOC_Day_01").getOrCreate() as spark:
     df.show()
 
     part_one_df = (
-        df.withColumn("part_one_list", func.collect_list("col1").over(window))
+        df.withColumn("list", func.collect_list("col1").over(window))
         .withColumn(
-            "part_one_result",
-            func.col("part_one_list")[0] < func.col("part_one_list")[1],
+            "result",
+            func.col("list")[0] < func.col("list")[1],
         )
-        .groupBy("part_one_result")
+        .groupBy("result")
         .count()
     )
     part_one_df.show()
